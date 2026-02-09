c6f7e15cae2b42307a24b02f982989097eab50d5
sachinsoni27 <sachinsoniofficial2003@gmail.com>
2026-02-09 21:21:20 +0530

    Testing contributions fix

--- Firstcodejava.java
+++ Firstcodejava.java
@@ -1,8 +1,12 @@
-public class Firstcodejava {
-    public static void main(String[] args) {
-System.out.println("Hello, World!");
-System.err.println("Hello, World!");
 
-    } 
+public class Firstcodejava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.err.println("Hello, World!");
+
+    }
 }
-wfq
